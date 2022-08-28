9c1edbfe753cf969aa708c7863029fb94a0aa804
LuzDaryLL <111297520+LuzDaryLL@users.noreply.github.com>
2022-08-28 15:10:49 -0500

    Delete src/proyecto directory
    Se elimina

--- src/proyecto/Principal.java
+++
@@ -1,9 +0,0 @@
-package proyecto;
-
-public class Principal {
-    public static void main(String[] args) {
-        Empresa emp1 = new Empresa("falabella","calle1 carrera1",123456,7890);
-        System.out.println(emp1);
-
-    }
-}
